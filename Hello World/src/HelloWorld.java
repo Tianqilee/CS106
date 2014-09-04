607e4809b2ad5cf42d71599f421d48746ed828e0
Li <tli1@cslab06.cs.skidmore.edu>
2014-09-03 20:06:17 -0400

    Initial commit.

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author tli1
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(" ---  ");
+		System.out.println("[   ]");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
