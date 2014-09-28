b4977fdbc3421df6a72d5e2ac0a1554eb0dcf9e4
Li <tli1@cslab06.cs.skidmore.edu>
2014-09-28 15:58:21 -0400

    Commit message here.

--- Project 2/src/CharacterTest.java
+++ Project 2/src/CharacterTest.java
@@ -61,6 +61,59 @@ public class CharacterTest {
 		String answer1 = console.nextLine();
 		if (answer1.equals("A")) {
 			disruptive++;
+		} else {
+			incremental++;
+		}
+		question4(console,disruptive,incremental);
+	}	public static void question4(Scanner console, int disruptive,
+			int incremental) {
+		System.out.println("question");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("What is your answer?");
+		String answer1 = console.nextLine();
+		if (answer1.equals("A")) {
+			disruptive++;
+		} else {
+			incremental++;
+		}
+		question5(console,disruptive,incremental);
+	}	public static void question5(Scanner console, int disruptive,
+			int incremental) {
+		System.out.println("question");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("What is your answer?");
+		String answer1 = console.nextLine();
+		if (answer1.equals("A")) {
+			disruptive++;
+		} else {
+			incremental++;
+		}
+		question6(console,disruptive,incremental);
+	}	public static void question6(Scanner console, int disruptive,
+			int incremental) {
+		System.out.println("question");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("What is your answer?");
+		String answer1 = console.nextLine();
+		if (answer1.equals("A")) {
+			disruptive++;
+		} else {
+			incremental++;
+		}
+		question7(console,disruptive,incremental);
+	}	
+	public static void question7(Scanner console, int disruptive,
+			int incremental) {
+		System.out.println("question");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("What is your answer?");
+		String answer1 = console.nextLine();
+		if (answer1.equals("A")) {
+			disruptive++;
 		} else  {
 			incremental++;
 		}
@@ -69,10 +122,14 @@ public class CharacterTest {
 public static void getResults(int disruptive, int incremental) {
 
 		// Determine the user's matching character base on the criteria.
-		if (disruptive > incremental) {
+		if (disruptive > incremental && disruptive>=6) {
 			System.out.print("You are A");
-		} else  {
+		} else if (disruptive > incremental && disruptive < 6) {
 			System.out.println("You are B");
+		} else if (incremental < disruptive && incremental >=6){
+			System.out.println("You are C");
+		}else if (incremental < disruptive && incremental <6){
+			System.out.println("You are D");
 		}
 	}
 }
