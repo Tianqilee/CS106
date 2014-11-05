213caedcdf650570a566e8bf0aac6dd91c44c7b2
Li <tli1@csr06.cs.skidmore.edu>
2014-11-04 19:06:28 -0500

    Added by Project 5.

--- Project 5/src/ClubAnalysis.java
+++ Project 5/src/ClubAnalysis.java
@@ -1,12 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 /**
- * @author Tianqi Li
- * A class to analyze the data and print results.
+ * @author Tianqi Li A class to analyze the data and print results.
  */
 public class ClubAnalysis {
-	//Set variables
+	// Set variables
 	static double ratingOfChelsea = 0.0;
 	static double ratingOfManchesterCity = 0.0;
 	static double ratingOfManchesterUnited = 0.0;
@@ -22,49 +22,50 @@ public class ClubAnalysis {
 
 	/**
 	 * A method to read file and analyze data.
-	 * @param throws FileNotFoundException
-	 *       If the file is not found.
+	 * 
+	 * @param throws FileNotFoundException If the file is not found.
 	 */
 	public void analyze() throws FileNotFoundException {
 		int numbLines = 0;
-		//Open input file.
+		// Open input file.
 		Scanner fileReader = new Scanner(new File("src/input2.txt"));
-		//Read each line of the file.
+		// Read each line of the file.
 		while (fileReader.hasNextLine()) {
 			fileReader.nextLine();
 			numbLines++;
 		}
-		System.out.println(numbLines);
-		//Close input file.
+		// Close input file.
 		fileReader.close();
-		//provision an array of ClubData objects to hold the values based on the number of lines in the file.
+		// provision an array of ClubData objects to hold the values based on
+		// the number of lines in the file.
 		ClubData[] clubData = new ClubData[numbLines];
-		//Populate the array from the file.
+		// Populate the array from the file.
 		int i = 0;
 		fileReader = new Scanner(new File("src/input2.txt"));
-while (fileReader.hasNext()) {
-	        //Get the position.
+		while (fileReader.hasNext()) {
+			// Get the position.
 			String position = fileReader.next();
-			//Get the club name.
+			// Get the club name.
 			String clubName = fileReader.next();
-			//If the club name is Manchester, also add the next word.
+			// If the club name is Manchester, also add the next word.
 			if (clubName.equals("Manchester")) {
 				clubName = clubName + "" + fileReader.next();
 			}
-			//Get the rating.
+			// Get the rating.
 			double rating = fileReader.nextDouble();
-			//Get the player name.
+			// Get the player name.
 			String playerName = fileReader.next();
-			//Get the age.
+			// Get the age.
 			double age = fileReader.nextDouble();
-			//Add a ClubData object to the array with the values we just pulled.
+			// Add a ClubData object to the array with the values we just
+			// pulled.
 			clubData[i] = new ClubData(position, clubName, rating, playerName,
 					age);
 			i++;
 		}
 		fileReader.close();
-		//Clarify and calculate the values.
-for (i = 0; i < numbLines; i++) {
+		// Clarify and calculate the values.
+		for (i = 0; i< clubData.length; i++) {
 			if (clubData[i].clubName.equals("Chelsea")) {
 				ratingOfChelsea += clubData[i].rating;
 				ageOfChelsea += clubData[i].age;
@@ -83,21 +84,27 @@ for (i = 0; i < numbLines; i++) {
 				countOfArsenal++;
 			}
 		}
-		double averageRatingC =ratingOfChelsea/countOfChelsea;
-		double averageAgeC= ageOfChelsea/countOfChelsea;
-		double averageRatingMC=ratingOfManchesterCity/countOfManchesterCity;
-		double averageAgeMC=ratingOfManchesterCity/countOfManchesterCity;
-		double averageRatingMU=ratingOfManchesterUnited/countOfManchesterUnited;
-		double averageAgeMU=ratingOfManchesterUnited/countOfManchesterUnited;
-		double averageRatingA=ratingOfManchesterCity/countOfManchesterCity;
-		double averageAgeA=ratingOfArsenal/countOfArsenal;
-       System.out.println("Average rating of Chelsea"+averageRatingC);
-       System.out.println("Average age of Chelsea" +averageAgeC);
-       System.out.println("Average rating of Manchester City" +averageRatingMC);
-       System.out.println("Average age of Manchester City" + averageAgeMC);
-       System.out.println("Average rating of Manchester United"+averageRatingMU);
-       System.out.println("Average age of Machester United" +averageAgeMU);
-       System.out.println("Average rating of Arsenal" +averageRatingA);
-       System.out.println("Average age of Arsenal" + averageAgeA);
+		//Set formulas for variables
+		float averageRatingC = (float) (ratingOfChelsea / countOfChelsea);
+		float averageAgeC = (float) ( ageOfChelsea / countOfChelsea);
+		float averageRatingMC = (float) (ratingOfManchesterCity / countOfManchesterCity);
+		float averageAgeMC = (float) (ageOfManchesterCity / countOfManchesterCity);
+		float averageRatingMU = (float) (ratingOfManchesterUnited
+				/ countOfManchesterUnited);
+		float averageAgeMU = (float) (ageOfManchesterUnited
+				/ countOfManchesterUnited);
+		float averageRatingA = (float) (ratingOfArsenal / countOfArsenal);
+		float averageAgeA = (float) (ageOfArsenal / countOfArsenal);
+		//Print results
+		System.out.println("Average rating of Chelsea:" +" "+ averageRatingC);
+		System.out.println("Average age of Chelsea:" + " " + averageAgeC);
+		System.out.println("Average rating of Manchester City:" +" " 
+				+ averageRatingMC);
+		System.out.println("Average age of Manchester City:" + " " + averageAgeMC);
+		System.out.println("Average rating of Manchester United:" + " " 
+				+ averageRatingMU);
+		System.out.println("Average age of Machester United:" + " " + averageAgeMU);
+		System.out.println("Average rating of Arsenal:" + " " + averageRatingA);
+		System.out.println("Average age of Arsenal:" + " " +averageAgeA);
 	}
 }
--- Project 5/src/Project5.java
+++ Project 5/src/Project5.java
@@ -1,19 +1,23 @@
 import java.io.FileNotFoundException;
+
 /**
- * @author Tianqi Li
- * A class containing a program to parse soccer team data and perform reporting
+ * @author Tianqi Li A class containing a program to parse soccer team data and
+ *         perform reporting
  */
 public class Project5 {
 
 	/**
 	 * Main method. Responsible for primary controller routing.
-	 * @param args Arguments passed to the program.
-	 * @throws FileNotFoundException 
+	 * 
+	 * @param args
+	 *            Arguments passed to the program.
+	 * @throws FileNotFoundException
+	 *             If file is not found.
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-    ClubAnalysis soccer = new ClubAnalysis();
-    soccer.analyze();
+		ClubAnalysis soccer = new ClubAnalysis();
+		soccer.analyze();
 	}
 
 }
